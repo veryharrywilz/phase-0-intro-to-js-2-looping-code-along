f43a0870f19ef390972479b2fac8617d8353c15e
Harrison <harriwilson5534@gmail.com>
2022-08-10 15:15:38 -0400

    commit tree 269f7d115a4af3c09332366856fef698bcceb5d8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const array = []
+    for (let i = 0; i < name.length; i++){
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        array.push(message);
+    }
+    return array;
+}
+
+function countDown(n){
+    let num = n
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
